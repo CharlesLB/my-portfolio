5c5d3a553c5e374a5df2a15459ae5e008f9d7872
Gabriella Carvalho <gabriellacarvalho0701@gmail.com>
2020-12-18 16:00:41 -0300

    :hatching_chick: seed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => 'gabriellacarvalho0701/portfolio',
+  ),
+  'versions' => 
+  array (
+    'gabriellacarvalho0701/portfolio' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+  ),
+);
